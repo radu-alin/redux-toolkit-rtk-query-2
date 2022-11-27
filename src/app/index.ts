1559d7eb1a83ca9b3596c98dd9cfcc6b0b668ffd
Alin RADU <cristian.alin.radu@reea.net>
2022-11-27 11:17:58 +0200

    implement rtk-thunk folder

--- src/app/index.ts
+++
@@ -1,3 +0,0 @@
-export { store } from './store';
-
-export { type AppDispatch, type RootState } from './store';
--- src/app/reducer.ts
+++
@@ -1,9 +0,0 @@
-import { usersReducer } from './../features/users/redux/usersSlice';
-import { postSliceReducer } from '../features/posts/redux/postsSlice';
-
-import { combineReducers } from '@reduxjs/toolkit';
-
-export const rootReducer = combineReducers({
-  posts: postSliceReducer,
-  users: usersReducer,
-});
